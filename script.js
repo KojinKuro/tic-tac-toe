4da77bb3f9b9f1896fcd082ecf2141ab2733869d
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-02-04 15:32:40 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function createGameBoard() {
+  var board = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+  ]
+
+  const getBoard = function() {
+    return board;
+  }
+
+  return { getBoard };
+}
+
+function createPlayer() {
+
+}
+
+const game = (function() {
+  
+
+  return {}
+})();
